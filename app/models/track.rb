2301bab0de6d55fda23303810581992e60ce404d
TannerEady <tanneready@shaw.ca>
2015-10-20 03:40:41 +0000

    Edit class Track.
    Add validations for title, url and author.

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,7 @@
 class Track < ActiveRecord::Base
 
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :url, presence: true, length: { maximum: 100 }
+  validates :author, presence: true, length: { maximum: 35 }
+
 end
