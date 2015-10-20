a9f5ebf42e94628ef289ab1f1547343a7b0afdd3
TannerEady <tanneready@shaw.ca>
2015-10-20 20:32:57 +0000

    Add validations for User class.
    Validate username, password and email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
 
   has_many :tracks
 
+  validates :username, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :password, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :email, presence: true
+
 end
