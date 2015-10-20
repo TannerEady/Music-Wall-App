aa1e11793f48f71f2d70a4cb4d7950d8e12ee2da
TannerEady <tanneready@shaw.ca>
2015-10-20 03:21:57 +0000

    Add actions to file.

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,26 @@
 get '/' do
   erb :index
 end
+
+get '/tracks' do 
+  @tracks = Track.all
+  erb :'tracks/index'
+end
+
+post '/tracks' do
+  @tracks = Track.new(
+    title:   params[:title],
+    url: params[:url],
+    author:  params[:author]
+  )
+  if @track.save
+    redirect '/tracks'
+  else
+    erb :'tracks/new'
+  end
+end
+
+get '/tracks/:id' do
+  @track = Track.find params[:id]
+  erb :'tracks/show'
+end
