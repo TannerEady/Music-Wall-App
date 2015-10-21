e059dda391f166d87f9139a8194dd5b5d625045b
TannerEady <tanneready@shaw.ca>
2015-10-21 01:23:17 +0000

    Add association with reviews.

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,5 +1,8 @@
 class Track < ActiveRecord::Base
 
+  belongs_to :user
+  has_many   :reviews
+
   validates :title, presence: true, length: { maximum: 50 }
   validates :url, presence: true, length: { maximum: 100 }
   validates :author, presence: true, length: { maximum: 35 }
