f314dc3d9607aec447fe2269c5e63254301858e9
TannerEady <tanneready@shaw.ca>
2015-10-21 00:51:41 +0000

    Edit custom validation.
    Change method name.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,9 +3,9 @@ class Vote < ActiveRecord::Base
   has_many :tracks
   has_many :users
 
-  validate :no_repeat_votes
+  validate :no_duplicate_votes
 
-  def no_repeat_votes
+  def no_duplicate_votes
     @votes = Vote.all
     @votes.each do |vote|
       if vote.track_id == track_id && vote.user_id == user_id
