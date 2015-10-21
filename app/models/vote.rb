6930766eef024a95aed01ab42fc80dc6a32c2261
TannerEady <tanneready@shaw.ca>
2015-10-21 00:49:26 +0000

    Add custom validation to Vote class.
    Ensure no duplicate votes.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,15 @@ class Vote < ActiveRecord::Base
   has_many :tracks
   has_many :users
 
+  validate :no_repeat_votes
+
+  def no_repeat_votes
+    @votes = Vote.all
+    @votes.each do |vote|
+      if vote.track_id == track_id && vote.user_id == user_id
+        errors.add(:user_id, "You already voted for this song. Nice try.")  
+      end
+    end
+  end
+
 end
